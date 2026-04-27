6ad5e27f68b8757e60172deff69804352002f41a
Daniel <167367785+u-iDaniel@users.noreply.github.com>
2026-04-27 10:01:04 -0600

    Finish assignment5

--- assignment5/user.cpp
+++ assignment5/user.cpp
@@ -3,24 +3,17 @@
 /**
  * Creates a new User with the given name and no friends.
  */
-User::User(const std::string& name)
-  : _name(name)
-  , _friends(nullptr)
-  , _size(0)
-  , _capacity(0)
-{
-}
+User::User(const std::string &name)
+    : _name(name), _friends(nullptr), _size(0), _capacity(0) {}
 
 /**
  * Adds a friend to this User's list of friends.
  * @param name The name of the friend to add.
  */
-void
-User::add_friend(const std::string& name)
-{
+void User::add_friend(const std::string &name) {
   if (_size == _capacity) {
     _capacity = 2 * _capacity + 1;
-    std::string* newFriends = new std::string[_capacity];
+    std::string *newFriends = new std::string[_capacity];
     for (size_t i = 0; i < _size; ++i) {
       newFriends[i] = _friends[i];
     }
@@ -34,32 +27,70 @@ User::add_friend(const std::string& name)
 /**
  * Returns the name of this User.
  */
-std::string
-User::get_name() const
-{
-  return _name;
-}
+std::string User::get_name() const { return _name; }
 
 /**
  * Returns the number of friends this User has.
  */
-size_t
-User::size() const
-{
-  return _size;
-}
+size_t User::size() const { return _size; }
 
 /**
  * Sets the friend at the given index to the given name.
  * @param index The index of the friend to set.
  * @param name The name to set the friend to.
  */
-void User::set_friend(size_t index, const std::string& name)
-{
+void User::set_friend(size_t index, const std::string &name) {
   _friends[index] = name;
 }
 
-/** 
+/**
  * STUDENT TODO:
- * The definitions for your custom operators and special member functions will go here!
+ * The definitions for your custom operators and special member functions will
+ * go here!
  */
+std::ostream &operator<<(std::ostream &out, const User &u) {
+  out << "User(name=" << u._name << ", friends=[";
+  for (int i = 0; i < u._size; i++) {
+    i < u._size - 1 ? out << u._friends[i] << ", " : out << u._friends[i];
+  }
+  out << "])";
+  return out;
+}
+
+User::~User() { delete[] this->_friends; }
+
+User::User(const User &user)
+    : _name{user._name}, _capacity{user._capacity}, _size{user._size},
+      _friends{new std::string[user._capacity]} {
+  for (int i = 0; i < _size; i++) {
+    _friends[i] = user._friends[i];
+  }
+}
+
+User &User::operator=(const User &user) {
+  if (this != &user) {
+    _name = user._name;
+    _size = user._size;
+    _capacity = user._capacity;
+
+    delete[] _friends; // Deallocate old memory
+    _friends = new std::string[_capacity];
+
+    for (int i = 0; i < _size; i++) {
+      _friends[i] = user._friends[i];
+    }
+  }
+  return *this;
+}
+
+User &User::operator+=(User &rhs) {
+  // Adds each other as friends
+  this->add_friend(rhs.get_name());
+  rhs.add_friend(this->get_name());
+  return *this;
+}
+
+bool User::operator<(const User &rhs) const {
+  // Compares by name
+  return this->_name < rhs._name;
+}
--- assignment5/user.h
+++ assignment5/user.h
@@ -6,23 +6,32 @@
 #include <iostream>
 #include <string>
 
-class User
-{
+class User {
 public:
-  User(const std::string& name);
-  void add_friend(const std::string& name);
+  User(const std::string &name);
+  void add_friend(const std::string &name);
   std::string get_name() const;
   size_t size() const;
-  void set_friend(size_t index, const std::string& name);
+  void set_friend(size_t index, const std::string &name);
 
-  /** 
+  /**
    * STUDENT TODO:
    * Your custom operators and special member functions will go here!
    */
+  friend std::ostream &operator<<(std::ostream &out, const User &u);
+
+  ~User();
+  User(const User &user);                // copy constructible
+  User &operator=(const User &user);     // copy assignment
+  User(User &&user) = delete;            // un-move constructed
+  User &operator=(User &&user) = delete; // un-move assigned
+
+  User &operator+=(User &rhs);
+  bool operator<(const User &rhs) const;
 
 private:
   std::string _name;
-  std::string* _friends;
+  std::string *_friends;
   size_t _size;
   size_t _capacity;
 };
