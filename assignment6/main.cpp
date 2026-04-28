8fcd4d9e4bd75764042b7dc3b061693be98b67f9
Daniel <167367785+u-iDaniel@users.noreply.github.com>
2026-04-28 13:52:20 -0600

    Finish assignment6

--- assignment6/main.cpp
+++ assignment6/main.cpp
@@ -9,14 +9,13 @@
 #include <vector>
 
 /** STUDENT_TODO: You will need to include a relevant header file here! */
-
 #include "autograder/utils.hpp"
+#include <optional>
 
 /**
  * A course. This should be familiar from Assignment 1!
  */
-struct Course
-{
+struct Course {
   std::string title;
   std::string number_of_units;
   std::string quarter;
@@ -25,25 +24,20 @@ struct Course
    * You don't have to ignore this anymore! We're defining the `==` operator for
    * the Course struct.
    */
-  bool operator==(const Course& other) const
-  {
+  bool operator==(const Course &other) const {
     return title == other.title && number_of_units == other.number_of_units &&
            quarter == other.quarter;
   }
 };
 
-class CourseDatabase
-{
+class CourseDatabase {
 public:
-  CourseDatabase(std::string filename)
-  {
+  CourseDatabase(std::string filename) {
     auto lines = read_lines(filename);
-    std::transform(lines.begin(),
-                   lines.end(),
-                   std::back_inserter(courses),
+    std::transform(lines.begin(), lines.end(), std::back_inserter(courses),
                    [](std::string line) {
                      auto parts = split(line, ',');
-                     return Course{ parts[0], parts[1], parts[2] };
+                     return Course{parts[0], parts[1], parts[2]};
                    });
   }
 
@@ -52,36 +46,47 @@ public:
    * @param course_title The title of the course to find.
    * @return You will need to figure this out!
    */
-  FillMeIn find_course(std::string course_title)
-  {
+  std::optional<Course> find_course(std::string course_title) {
     /* STUDENT_TODO: Implement this method! You will need to change the return
      * type. */
+    for (Course c : courses) {
+      if (c.title == course_title)
+        return c;
+    }
+    return std::nullopt; // or return {}
   }
 
 private:
   std::vector<Course> courses;
 };
 
-int
-main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
   static_assert(
-    !std::is_same_v<std::invoke_result_t<decltype (&CourseDatabase::find_course), 
-                      CourseDatabase, std::string>,
-                    FillMeIn>,
-    "You must change the return type of CourseDatabase::find_course to "
-    "something other than FillMeIn.");
+      !std::is_same_v<
+          std::invoke_result_t<decltype(&CourseDatabase::find_course),
+                               CourseDatabase, std::string>,
+          FillMeIn>,
+      "You must change the return type of CourseDatabase::find_course to "
+      "something other than FillMeIn.");
 
   if (argc == 2) {
     CourseDatabase db("autograder/courses.csv");
     auto course = db.find_course(argv[1]);
-    
-    /******************************************************** 
+
+    /********************************************************
     STUDENT_TODO: Populate the output string with the right information to print
     Please pay special attention to the README here
     ********************************************************/
 
-    std::string output = /* STUDENT_TODO */
+    std::string output = course
+                             .transform([](Course c) -> std::string {
+                               return "Found course: " + c.title + "," +
+                                      c.number_of_units + "," + c.quarter;
+                             })
+                             .or_else([]() -> std::optional<std::string> {
+                               return "Course not found.";
+                             })
+                             .value();
 
     /********************************************************
      DO NOT MODIFY ANYTHING BELOW THIS LINE PLEASE
@@ -90,6 +95,6 @@ main(int argc, char* argv[])
     std::cout << output << std::endl;
     return 0;
   }
-  
+
   return run_autograder();
 }
